9d74923e1f8c6aa21ff74020a26f8ada0c6e3c70
Kyle Jacobson <jacokyle01@gmail.com>
2024-01-27 23:33:02 -0600

    setup user initialization and storage in database

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "users.json");
 dotenv.config();
 
-const { Client } = require('discord.js');
+const { Client } = require("discord.js");
 const client = new Client({
-    intents: [
-        'DirectMessages',
-        'GuildMembers',
-        'Guilds',
-        'GuildMessages'
-    ]
-})
+	intents: ["DirectMessages", "GuildMembers", "Guilds", "GuildMessages"],
+});
+
+function readUsers() {
+	try {
+		const data = fs.readFileSync(filePath, "utf8");
+		return JSON.parse(data);
+	} catch (error) {
+		return [];
+	}
+}
+
+function initializeUser(uid) {
+	const users = readUsers();
+    const user = {
+        "id": uid,
+        "standing": -1,
+        "streak": 0,
+        challenging: [],
+        challengedBy: []
+    }
+    users.push(user);
+	writeUsers(users);
+}
+
+function writeUsers(users) {
+	fs.writeFileSync(filePath, JSON.stringify(users, null, 2), "utf8");
+}
 
 client.login(process.env.DISCORD_TOKEN);
 client.on("messageCreate", async (message) => {
-    console.log(message);
+	console.log(message);
+    initializeUser(message.author.id);
 });
