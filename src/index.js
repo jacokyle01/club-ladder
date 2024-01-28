2273f00250f9ff996d976d0e28ccbe1399cdb0c1
Kyle Jacobson <jacokyle01@gmail.com>
2024-01-28 12:27:19 -0600

    send personalized messages to users

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
 const filePath = path.join(__dirname, "users.json");
-dotenv.config();
 
+dotenv.config();
+const url = "https://discord.com/channels/1201006010739990693";
 const { Client } = require("discord.js");
 const client = new Client({
-	intents: ["DirectMessages", "GuildMembers", "Guilds", "GuildMessages"],
+	intents: [
+		"DirectMessages",
+		"GuildMembers",
+		"Guilds",
+		"GuildMessages",
+		"MessageContent",
+	],
 });
 
 function readUsers() {
@@ -20,14 +27,14 @@ function readUsers() {
 
 function initializeUser(uid) {
 	const users = readUsers();
-    const user = {
-        "id": uid,
-        "standing": -1,
-        "streak": 0,
-        challenging: [],
-        challengedBy: []
-    }
-    users.push(user);
+	const user = {
+		id: uid,
+		standing: -1,
+		streak: 0,
+		challenging: [],
+		challengedBy: [],
+	};
+	users.push(user);
 	writeUsers(users);
 }
 
@@ -37,11 +44,29 @@ function writeUsers(users) {
 
 client.login(process.env.DISCORD_TOKEN);
 client.on("messageCreate", async (message) => {
-	console.log(message);
-    const users = readUsers();
-    //initialize user if necessary
-    if (!users.find(user => user.id == message.author.id)) {
-        initializeUser(message.author.id);
-    }
+    const cid = '1201006010739990693';
+    const channel = client.channels.cache.get(cid);
 
+	console.log(message);
+	const users = readUsers();
+
+	//initialize user if necessary
+	if (!users.find((user) => user.id == message.author.id)) {
+		initializeUser(message.author.id);
+	}
+
+	if (message.content.startsWith("!")) {
+
+        const users = readUsers();
+        const me = users.find((user) => user.id == message.author.id);
+		const command = message.content.split(" ").at(0).substring(1);
+		console.log(command);
+
+
+		switch (command) {
+			case "stats":
+                channel.send(JSON.stringify(me.streak));
+				break;
+		}
+	}
 });
