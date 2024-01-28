c3bbce3a452b7b897dca3bcb2ec9747b4c792638
Kyle Jacobson <jacokyle01@gmail.com>
2024-01-28 17:43:07 -0600

    refactor, only read/write once

--- src/index.js
+++ src/index.js
@@ -25,8 +25,7 @@ function readUsers() {
 	}
 }
 
-function initializeUser(uid) {
-	const users = readUsers();
+function initializeUser(users, uid) {
 	const user = {
 		id: uid,
 		standing: users.length + 1,
@@ -35,19 +34,17 @@ function initializeUser(uid) {
 		challengedBy: [],
 		alias: "",
 	};
-	users.push(user);
-	writeUsers(users);
+	return user;
 }
 
-function updateUser(userId, updatedData) {
-	const users = readUsers();
-	const index = users.findIndex((user) => user.id === userId);
+// function updateUser(userId, updatedData) {
+// 	const index = users.findIndex((user) => user.id === userId);
 
-	if (index !== -1) {
-		users[index] = { ...users[index], ...updatedData };
-		writeUsers(users);
-	}
-}
+// 	if (index !== -1) {
+// 		users[index] = { ...users[index], ...updatedData };
+// 		writeUsers(users);
+// 	}
+// }
 
 function writeUsers(users) {
 	fs.writeFileSync(filePath, JSON.stringify(users, null, 2), "utf8");
@@ -59,22 +56,29 @@ const prettyIds = (users, ids) => {
 	return pretty;
 };
 
+const findIndexById = (users, id) => users.findIndex((user) => user.id == id);
+const userFromId = (users, id) => users.find((user) => user.id == id);
+
 client.login(process.env.DISCORD_TOKEN);
 client.on("messageCreate", async (message) => {
+	const users = readUsers();
 	const cid = "1201006010739990693";
 	const channel = client.channels.cache.get(cid);
-
-	console.log(message);
-	const users = readUsers();
-
+	// client.channels
+	// 	.fetch(cid)
+	// 	.then((channel) => channel.send("booyah!"));
+	console.log("hi");
 	//initialize user if necessary
-	if (!users.find((user) => user.id == message.author.id)) {
-		initializeUser(message.author.id);
+	const senderId = message.author.id;
+	console.log("sender ID " + senderId);
+	if (findIndexById(users, senderId) == -1) {
+		console.log("initializing");
+		users.push(initializeUser(users, senderId));
 	}
-
+	console.log("USERS " + users);
 	if (message.content.startsWith("!")) {
-		const users = readUsers();
-		const me = users.find((user) => user.id == message.author.id);
+		let me = userFromId(users, senderId);
+		console.log("ME" + me.challengedBy);
 		const command = message.content.split(" ").at(0).substring(1);
 		const args = message.content.split(" ");
 		args.shift();
@@ -83,42 +87,59 @@ client.on("messageCreate", async (message) => {
 
 		switch (command) {
 			case "stats":
+				console.log("STATS");
+				console.log(channel.id);
 				channel.send(
 					"Alias " +
 						me.alias +
 						"\nStanding: " +
 						me.standing +
 						"\nStreak: " +
-						me.streak + 
-                        "\nChallenging: " +
-                        prettyIds(users, me.challenging) +
-                        "\nChallenged by " + 
-                        prettyIds(users, me.challengedBy)
+						me.streak +
+						"\nChallenging: " +
+						prettyIds(users, me.challenging) +
+						"\nChallenged by " +
+						prettyIds(users, me.challengedBy)
 				);
+				console.log("hi sher");
 				break;
 			case "challenge": // ex. !challenge @Presiident
 			case "vs":
-				channel.send("⚔️  Challenging " + args[0] + " ⚔️");
-				//assume usage is "!challenge @[username]"
+				//assume usage is "!challenge @[username] and username is initialized"
 				const numbers = args[0].match(/\d+/g);
 				//TODO null safety here
-				const challengedId = numbers[0];
-				const challengee = users.find((user) => user.id == challengedId);
-				console.log(challengedId);
+				const challengeeId = numbers[0];
+				let challengee = userFromId(users, challengeeId);
 
+				//don't challenge the same person twice
+				if (me.challenging.includes(challengeeId)) {
+					channel.send("You are already challenging this person");
+					break;
+				}
+
+				channel.send("⚔️  Challenging " + args[0] + " ⚔️");
 				//update challenger and challengee
-				updateUser(me.id, {
-					challenging: [...me.challenging, challengedId],
-				});
-				updateUser(challengedId, {
-					challengedBy: [...challengee.challengedBy, me.id],
-				});
+				users[findIndexById(users, me.id)] = {
+					...me,
+					challenging: [...me.challenging, challengeeId],
+				};
+
+				users[findIndexById(users, challengeeId)] = {
+					...challengee,
+					challenged: [...challengee.challengedBy, me.id],
+				};
+				console.log(challengee);
 				break;
 
 			case "alias":
 				if (args[0]) {
-					updateUser(me.id, { alias: args[0] });
+					users[findIndexById[senderId]] = {
+                        ...me, 
+                        alias: args[0]
+                    }
 				}
+				break;
 		}
 	}
+	writeUsers(users);
 });
